2ba5c585bdc5debc1de9b7c39e380996a92adcb4
aodianjun <130821781+aodianjun@users.noreply.github.com>
2026-09-14 08:37:24 +0800

    chore(release): 111-0.3.84

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.aodianjun.hyperglow.cnplus"
         minSdk = 33
         targetSdk = 37
-        versionCode = 110
-        versionName = "0.3.83"
+        versionCode = 111
+        versionName = "0.3.84"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String",
