1d190067bc564859e56b578016d5c9a4f03e8db1
aodianjun <aodianjun@users.noreply.github.com>
2026-09-16 02:02:21 +0000

    chore: 升级版本号至 0.3.88 / versionCode 115（发布预发行）

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.aodianjun.hyperglow.cnplus"
         minSdk = 33
         targetSdk = 37
-        versionCode = 114
-        versionName = "0.3.87"
+        versionCode = 115
+        versionName = "0.3.88"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String",
