458e86837134c7a334501a6c905b1ab9ea9e65b2
aodianjun <aodianjun@users.noreply.github.com>
2026-09-19 23:29:42 +0000

    chore: bump to 0.3.97 (124)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.aodianjun.hyperglow.cnplus"
         minSdk = 33
         targetSdk = 37
-        versionCode = 123
-        versionName = "0.3.96"
+        versionCode = 124
+        versionName = "0.3.97"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String",
