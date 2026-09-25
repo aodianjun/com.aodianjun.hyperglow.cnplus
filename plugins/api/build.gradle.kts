0c195269ef9994b27577a367df72d0d42c839c45
aodianjun <aodianjun@users.noreply.github.com>
2026-09-25 03:15:45 +0000

    feat: update workspace
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- plugins/api/build.gradle.kts
+++ plugins/api/build.gradle.kts
@@ -4,3 +4,14 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
 }
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
--- plugins/demo/build.gradle.kts
+++ plugins/demo/build.gradle.kts
@@ -12,6 +12,17 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     compileOnly(project(":plugins:api"))
 }
