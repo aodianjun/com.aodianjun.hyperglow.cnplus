a178c20ecf9f4fb0f2e298740d61e39d3af774d6
凹点菌 <130821781+aodianjun@users.noreply.github.com>
2026-09-27 01:04:41 +0800

    chore(release): bump version to 0.3.120 (147)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.aodianjun.hyperglow.cnplus"
         minSdk = 33
         targetSdk = 37
-        versionCode = 146
-        versionName = "0.3.119"
+        versionCode = 147
+        versionName = "0.3.120"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
             "String",
